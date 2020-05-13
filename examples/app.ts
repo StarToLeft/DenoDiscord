cb45e5b4bc873d1fe8750d81f05af62ad32f17fa
Anton Hagser <melonsmurf@epsidel.se>
2020-05-14 00:27:58 +0200

    Updated and refactoring, working on Interfaces implementations

--- examples/app.ts
+++ examples/app.ts
@@ -1,5 +1,13 @@
-import Discord from "../src/Discord.ts";
+import { Discord, ReadyEvent } from "../mod.ts";
+import Globals from "../src/Globals.ts";
 
-const app = new Discord("");
+const app = new Discord.Client();
 
-app.login();
+Globals.getInstance().Debug = true;
+
+app.on("READY", (e: ReadyEvent) => {
+    console.log(`${e.data.Client.username}#${e.data.Client.discriminator}`);
+})
+
+//app.login("NDE5MjUxMDYyOTY5NDY2ODgw.XrxtiA.6KWCPIeWc7bsYHCs5Y6CTmQ4uL8"); //
+app.login("NzEwMTcxOTk0ODIxMzYxNzE2.XrwlQw.9Ck_SDfepR7S09p1ZpyM9l9mpIY");
